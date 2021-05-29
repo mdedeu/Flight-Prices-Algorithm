e29eaf5e3773bc423dc93fb6d1e61dc61be12804
Marcos Dedeu <mdedeu@itba.edu.ar>
2021-05-29 14:22:28 -0300

    a few comments in Ejercicio1

--- src/main/java/Ejercicio1.java
+++ src/main/java/Ejercicio1.java
@@ -4,20 +4,21 @@ public class Ejercicio1 {
 
   static int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
     Map<Integer, List<Edge>> nodes = new HashMap<>();
-    // Setea los nodos y aristas del grafo (undirected)
+    //Set a map where each node has a list of edges
     for (int i = 0; i < n; i++)
       nodes.put(i, new ArrayList<>());
-
+    //initialize map
     for (int[] flight : flights) {
       nodes.get(flight[0]).add(new Edge(flight[1], flight[2],0));
     }
-
+    //get least expensive path with k or less stops
     return dijkstra(src,dst, nodes, k);
   }
   private static int dijkstra(int src,int dest, Map<Integer, List<Edge>> nodes,int k) {
-    int minCost=-1;
-    Map<Integer,Integer> distances = new HashMap<>();
+    int minCost = -1;
+    Map<Integer, Integer> distances = new HashMap<>();
     PriorityQueue<Edge> queue = new PriorityQueue<>();
+
     queue.add(new Edge(src, 0,0));
     while (!queue.isEmpty()) {
       Edge lastEdge = queue.remove();
@@ -38,7 +39,7 @@ public class Ejercicio1 {
   static class Edge implements Comparable<Edge> {
     int target;
     int weight;
-    int length;
+    int length; //length of the path till then
     public Edge(int target, int weight, int length) {
       this.target = target;
       this.weight = weight;
